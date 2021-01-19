539cfb82dd594cf7a6535ccc62a1fe2ceddcb091
VuXuanThanh-Dev <devxuanthanhvu0503@gmail.com>
2021-01-19 19:52:25 +0700

    all file

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+// js
+
+$(document).ready(()=>{
+    // $(window).scroll(function(){
+    //     $('.addvertise-img').animate({
+    //         top:$(window).offset().top
+    //     },500);
+    // });
+    // let arrImg = [
+    //     "../images/slider1.jpg","../images/slider2.png","../images/slider3.png"
+    // ]
+    //  let i = 0;
+    // let slider = setInterval(()=>{
+    //    i++;
+    //     $('.slider-img').fadeOut(500,()=>{
+    //         $('.slider-img').attr('src',arrImg[i]);
+    //     });
+    //     $('.slider-img').fadeIn(500);
+        
+    //     if(i>=2){
+    //         i=0;
+    //     }
+    // },2500);
+    
+});
